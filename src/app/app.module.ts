04271b1768e54e2178dfa0017c679f412c110df2
Andres <andreso@ax-inbox.com>
2022-06-20 22:21:03 -0500

    incial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AuthModule } from '@auth0/auth0-angular';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import { PreciosComponent } from './components/precios/precios.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule,
+     // Import the module into the application, with configuration
+     AuthModule.forRoot({
+      domain: 'dev-miv9gtn8.us.auth0.com',
+      clientId: '3Sg5zgrkWXdjn4HneV4WqFTptsJHO8tv'
+    }),
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
